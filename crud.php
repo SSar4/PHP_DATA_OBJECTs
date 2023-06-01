3937440aadd9a6465e67460afc66109369c892cf
j.a.s <aras.strong8@gmail.com>
2023-06-01 20:41:43 -0300

    listagem de usuario

--- crud.php
+++ crud.php
@@ -7,10 +7,35 @@
         $sql->execute(array($_POST['nome'],$_POST['email']));
         echo 'inserted';
     }
+
+    // Listagem de usuários
+    $consulta = $pdo->query("SELECT * FROM usuarios");
+    $usuarios = $consulta->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+<!-- Formulário de inserção -->
 <form method="post">
     <input type="text" name="nome">
     <input type="text" name="email">
     <input type="submit" name="enviar">
 </form>
+
+<!-- Listagem de usuários -->
+<table>
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($usuarios as $usuario) : ?>
+            <tr>
+                <td><?php echo $usuario['id']; ?></td>
+                <td><?php echo $usuario['nome']; ?></td>
+                <td><?php echo $usuario['email']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
